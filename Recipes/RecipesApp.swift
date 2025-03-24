afc4be65aae7772538aa24a55d8cf3c91cca5472
NutMegCode <madwyer@hotmail.com>
2025-03-24 14:22:55 +0800

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Meg on 24/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
